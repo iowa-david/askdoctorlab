f2966d5b288f047ad72427ca6c6177bbf2a989c4
iowa-david <148511573+iowa-david@users.noreply.github.com>
2024-04-28 20:13:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
-import('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
